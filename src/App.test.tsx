cdeb1de77d151003acba16af9644e4609fd01502
Mickey1992 <wl920425@gmail.com>
2022-05-22 22:49:57 +0900

    add new folder

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './AlbumList';
+import App from './pages/albums/AlbumList';
 
 test('renders learn react link', () => {
   render(<App />);
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import AlbumList from './AlbumList';
+import AlbumList from './pages/albums/AlbumList';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
