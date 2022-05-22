74468001599f69aea47d48a9aaff694afc0be00f
Mickey1992 <wl920425@gmail.com>
2022-05-23 00:25:34 +0900

    add color for header

--- src/pages/common/WebsiteHeader.tsx
+++ src/pages/common/WebsiteHeader.tsx
@@ -1,5 +1,5 @@
 import './WebsiteHeader.css';
 
 export default function WebpageHeader () {
-    return <div id="website-header">KinKi Land</div>
+    return <div id="website-header"><span className="blue">KinKi</span> <span className="red">Land</span></div>
 }
