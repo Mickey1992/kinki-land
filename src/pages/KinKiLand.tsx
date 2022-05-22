f39c003f673da8ce2d284ace61f9e5dab6c89bc9
Mickey1992 <wl920425@gmail.com>
2022-05-22 23:08:54 +0900

    add header for my page

--- src/pages/KinKiLand.tsx
+++ src/pages/KinKiLand.tsx
@@ -1,5 +1,11 @@
 import AlbumList from "./albums/AlbumList";
+import WebpageHeader from "./common/WebsiteHeader";
 
 export default function KinKiLand () {
-    return <AlbumList />
+    return (
+        <>
+            <WebpageHeader />
+            <AlbumList />
+        </>
+    )
 }
---
+++ src/pages/common/WebsiteHeader.tsx
@@ -0,0 +1,5 @@
+import './WebsiteHeader.css';
+
+export default function WebpageHeader () {
+    return <div id="website-header">KinKi Land</div>
+}
