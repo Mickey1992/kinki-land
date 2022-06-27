7e80c7656f3de24f73ee6e992bf2a3b37ec6b62e
Mickey1992 <wl920425@gmail.com>
2022-06-27 19:48:29 +0900

    connect my app to the browser's URL

--- src/pages/KinKiLand.tsx
+++ src/pages/KinKiLand.tsx
@@ -1,11 +1,12 @@
+import { BrowserRouter } from "react-router-dom";
 import AlbumList from "./AlbumList/AlbumList";
 import WebpageHeader from "./common/WebsiteHeader";
 
 export default function KinKiLand () {
     return (
-        <>
+        <BrowserRouter>
             <WebpageHeader />
             <AlbumList />
-        </>
+        </BrowserRouter>
     )
 }
