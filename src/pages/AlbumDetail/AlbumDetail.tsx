51dcca60631eec1ccb18ffee95c3360373584d82
Mickey1992 <wl920425@gmail.com>
2022-06-29 17:44:49 +0900

    show large photo when click photo on Album-Detail page

--- src/pages/AlbumDetail/AlbumDetail.tsx
+++ src/pages/AlbumDetail/AlbumDetail.tsx
@@ -1,22 +1,26 @@
+import { useEffect, useState } from "react";
 import { AlbumNode, ROOT_ALBUM_FOLDER_PATH } from "../AlbumList/AlbumList";
 import Album from "./Album";
-import Photo from "./Photo";
+import Photo, { LargePhoto } from "./Photo";
 
 
 export default function AlbumDetail (props: {path: string, albumNodes: AlbumNode[]}) {
     const folders = props.path.replace(ROOT_ALBUM_FOLDER_PATH, "").split("/").slice(1);
+    const [largePhotoName, setLargePhotoName] = useState<string|null>(null);
+    
     return (
         <>
             <h3>{folders.join(" > ")}</h3>
             {
                 props.albumNodes.map(node => {     
                     if(typeof node === "string"){
-                        return <Photo key={node} folderName={props.path} fileName={node} />
+                        return <Photo key={node} folderName={props.path} fileName={node} setLargePhotoSrc={setLargePhotoName}/>
                     }
                     const newPath = props.path+"/"+node.folderName;
                     return <Album key={node.folderName} path={newPath} album={node}/>
                 })
             }
+            {largePhotoName && <LargePhoto folderName={props.path} fileName={largePhotoName} />}
         </>
     )
 }
--- src/pages/AlbumDetail/Photo.tsx
+++ src/pages/AlbumDetail/Photo.tsx
@@ -1,9 +1,25 @@
 import './Photo.css'
+interface PhotoProps {
+    folderName: string, 
+    fileName: string,
+    setLargePhotoSrc?: React.Dispatch<React.SetStateAction<string | null>>
+}
 
-export default function Photo(props: {folderName: string, fileName: string}) {
+export default function Photo(props: PhotoProps) {
     return (
-        <div className="photo">
+        <div className="photo"  onClick={() => props.setLargePhotoSrc!(props.fileName)}>
             <img src={props.folderName+"/"+props.fileName} alt={props.fileName} />
+        </div>
+    )
+}
+
+export function LargePhoto(props: PhotoProps) {
+    return (
+        <div className="large-photo">
+            <div className='large-photo-background'></div>
+            <div className="large-photo-img">
+                <img src={props.folderName+"/"+props.fileName} alt={props.fileName} />
+            </div>
         </div>
     )
 }
