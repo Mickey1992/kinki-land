528053013e1916f0ee11e344c496aa203bc3bcf7
Mickey1992 <wl920425@gmail.com>
2022-08-05 18:57:19 +0900

    add log4j2 configuration

--- src/main/java/com/kinki/land/app/controller/TestController.java
+++ src/main/java/com/kinki/land/app/controller/TestController.java
@@ -1,5 +1,8 @@
 package com.kinki.land.app.controller;
 
+import com.kinki.land.app.Application;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class TestController {
+    private static final Logger logger = LogManager.getLogger(TestController.class);
+
     @GetMapping("hello")
     public String hello() {
+        logger.error("Hello World");
         return "hello controller";
     }
 }
